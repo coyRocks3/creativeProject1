a60c10b2e1ea49fa171c8abc0af10fee77d6c77a
Franchesca Uy <franchesca_u97@yahoo.com>
2018-09-18 21:19:59 +0000

    added page functionality

---
+++ mainscript.js
@@ -0,0 +1,109 @@
+function pageDefault() {
+    var getStart = document.getElementById("getStartedDisplay");
+    var whatNeeded = document.getElementById("whatYouNeedDisplay");
+    var firstStep = document.getElementById("step1Display");
+    var secondStep = document.getElementById("step2Display");
+    var thirdStep = document.getElementById("step3Display");
+    var fourthStep = document.getElementById("step4Display");
+    
+    getStart.style.display = "block";
+    whatNeeded.style.display = "none";
+    firstStep.style.display = "none";
+    secondStep.style.display = "none";
+    thirdStep.style.display = "none";
+    fourthStep.style.display = "none";
+}
+
+function getStarted() {
+    var getStart = document.getElementById("getStartedDisplay");
+    var whatNeeded = document.getElementById("whatYouNeedDisplay");
+    var firstStep = document.getElementById("step1Display");
+    var secondStep = document.getElementById("step2Display");
+    var thirdStep = document.getElementById("step3Display");
+    var fourthStep = document.getElementById("step4Display");
+    
+    getStart.style.display = "block";
+    whatNeeded.style.display = "none";
+    firstStep.style.display = "none";
+    secondStep.style.display = "none";
+    thirdStep.style.display = "none";
+    fourthStep.style.display = "none";
+
+
+}
+
+function whatYouNeed() {
+    var getStart = document.getElementById("getStartedDisplay");
+    var whatNeeded = document.getElementById("whatYouNeedDisplay");
+    var firstStep = document.getElementById("step1Display");
+    var secondStep = document.getElementById("step2Display");
+    var thirdStep = document.getElementById("step3Display");
+    var fourthStep = document.getElementById("step4Display");
+    whatNeeded.style.display = "block";
+    getStart.style.display = "none";
+    firstStep.style.display = "none";
+    secondStep.style.display = "none";
+    thirdStep.style.display = "none";
+    fourthStep.style.display = "none";
+        
+}
+
+function step1() {
+    var getStart = document.getElementById("getStartedDisplay");
+    var whatNeeded = document.getElementById("whatYouNeedDisplay");
+    var firstStep = document.getElementById("step1Display");
+    var secondStep = document.getElementById("step2Display");
+    var thirdStep = document.getElementById("step3Display");
+    var fourthStep = document.getElementById("step4Display");
+    whatNeeded.style.display = "none";
+    getStart.style.display = "none";
+    firstStep.style.display = "block";
+    secondStep.style.display = "none";
+    thirdStep.style.display = "none";
+    fourthStep.style.display = "none";
+}
+
+function step2() {
+    var getStart = document.getElementById("getStartedDisplay");
+    var whatNeeded = document.getElementById("whatYouNeedDisplay");
+    var firstStep = document.getElementById("step1Display");
+    var secondStep = document.getElementById("step2Display");
+    var thirdStep = document.getElementById("step3Display");
+    var fourthStep = document.getElementById("step4Display");
+    whatNeeded.style.display = "none";
+    getStart.style.display = "none";
+    firstStep.style.display = "none";
+    secondStep.style.display = "block";
+    thirdStep.style.display = "none";
+    fourthStep.style.display = "none";
+}
+
+function step3() {
+    var getStart = document.getElementById("getStartedDisplay");
+    var whatNeeded = document.getElementById("whatYouNeedDisplay");
+    var firstStep = document.getElementById("step1Display");
+    var secondStep = document.getElementById("step2Display");
+    var thirdStep = document.getElementById("step3Display");
+    var fourthStep = document.getElementById("step4Display");
+    whatNeeded.style.display = "none";
+    getStart.style.display = "none";
+    firstStep.style.display = "none";
+    secondStep.style.display = "none";
+    thirdStep.style.display = "block";
+    fourthStep.style.display = "none";
+}
+
+function step4() {
+    var getStart = document.getElementById("getStartedDisplay");
+    var whatNeeded = document.getElementById("whatYouNeedDisplay");
+    var firstStep = document.getElementById("step1Display");
+    var secondStep = document.getElementById("step2Display");
+    var thirdStep = document.getElementById("step3Display");
+    var fourthStep = document.getElementById("step4Display");
+    whatNeeded.style.display = "none";
+    getStart.style.display = "none";
+    firstStep.style.display = "none";
+    secondStep.style.display = "none";
+    thirdStep.style.display = "none";
+    fourthStep.style.display = "block";
+}
